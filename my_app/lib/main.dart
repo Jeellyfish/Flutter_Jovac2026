552bf88d66940054dff4df97f8753d7f795a0383
Jeellyfish <pranavkumarverma6@gmail.com>
2026-07-01 11:04:22 +0530

    till wrap widget

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,6 +1,15 @@
 import "package:flutter/material.dart";
 import "screen/stateless_widgets.dart";
 import "screen/statefull_widget.dart";
+import "screen/container_widget.dart";
+import "screen/safearea_widget.dart";
+import "screen/row_column_widgets.dart";
+import "screen/stack_widget.dart";
+import "screen/spacer.dart";
+import "screen/align_widget.dart";
+import "screen/wrap_widget.dart";
+
+
 void main(){
   runApp(MyApp());
 }
@@ -13,7 +22,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //home: const HomeScreen(),
-      home: const HomeScreen1(),
+      //home: const HomeScreen1(),
+      // home:ContainerWidget(),
+      // home:SafeAreaWidget(),
+      // home:RowColumnWidgets(),
+      // home:StackWidget(),
+      // home:SpacerWidget(),
+      // home:AlignWidget(),
+      home:WrapWidget(),
     );
   }
 }
